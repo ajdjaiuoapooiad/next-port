ae411bff03056b1f7a741e0ff3377782825fea8d
yukito <iyggf66974@yahoo.ne.jp>
2025-03-01 16:46:44 +0900

    JobListページ　デザインok

---
+++ app/(dashboard)/posts/page.jsx
@@ -0,0 +1,33 @@
+import prisma from '@/utils/db'
+import React from 'react'
+
+const PostsList = async () => {
+
+  const jobs = await prisma.jobs.findMany()
+  return (
+    <div className='grid grid-cols-5'>
+      <div className='col-span-1'> 
+        <h1>Sidebar</h1>
+      </div>
+
+      <div className='col-span-4'>
+
+      <h1>Posts List</h1>
+        <div className='grid grid-cols-3 '>
+          {jobs.map((post) => {
+            return (
+              <div key={post.id} className='border p-4 my-4 mx-3 col-span-1'>
+                <h1>{post.title}</h1>
+                <p>{post.description}</p>
+              </div>
+            )
+          })}
+          
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default PostsList
