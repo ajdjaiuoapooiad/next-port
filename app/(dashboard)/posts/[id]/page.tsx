73e6c7136ed19efe408655fc157a397452ab55c1
yukito <iyggf66974@yahoo.ne.jp>
2025-03-02 13:33:22 +0900

    EditPage error 修正

--- app/(dashboard)/posts/[id]/page.tsx
+++ app/(dashboard)/posts/[id]/page.tsx
@@ -2,17 +2,39 @@
 
 import Sidebar from '@/components/Sidebar'
 import React, { useEffect } from 'react'
-import { getSingleJobAction } from '@/utils/actions'
+import { getSingleJobAction, updateJobAction } from '@/utils/actions'
 import EditJobForm from '@/components/EditJobForm'
+import prisma from '@/utils/db'
+import { JobType } from '@/utils/type'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { z } from 'zod'
+import { formSchema } from '../../create-post/page'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 
 const DetailPage =  ({ params }: { params: { id: number } }) => {
 
-  useEffect(() => {
-    getSingleJobAction(params.id) 
-  }
-  , []) 
 
+  // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        title: "",
+        description: "",
+      },
+    })
+   
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+      // Do something with the form values.
+      // ✅ This will be type-safe and validated.
+      const { title, description } = values
+      updateJobAction(params.id,{ title, description })
+      console.log(values)
+    }
 
   return (
     <div className='grid grid-cols-5'>
@@ -23,7 +45,40 @@ const DetailPage =  ({ params }: { params: { id: number } }) => {
       <div className='col-span-4'>
         <h1>Detail Page</h1>
         {/* Add content here */}
-        
+        <Form {...form}  >
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-bold">Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-bold">Description</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" >投稿</Button>
+          </form>
+        </Form>
       </div>
     </div>
   )
