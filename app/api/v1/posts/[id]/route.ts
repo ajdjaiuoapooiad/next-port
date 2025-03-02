de00613a735ae72f197ff79c9fc2c8e99b535e81
yukito <iyggf66974@yahoo.ne.jp>
2025-03-02 18:37:32 +0900

    API delete update ok

--- app/api/v1/posts/[id]/route.ts
+++ app/api/v1/posts/[id]/route.ts
@@ -13,3 +13,30 @@ export const GET = async (
     })
     return NextResponse.json(post)
 }
+
+export const PUT = async (
+    req: Request, 
+    {params}: {params: {id: number}}
+) =>  {
+    const id = params.id
+    const { title, description } = await req.json()
+
+    const post = await prisma.jobs.update({
+        where: { id: Number(id)},
+        data: {
+            title,
+            description,
+        }
+    })
+    return NextResponse.json(post)
+}
+export const DELETE = async (
+    req: Request, 
+    {params}: {params: {id: number}}
+) =>  {
+    const id = params.id
+    const post = await prisma.jobs.delete({
+        where: { id: Number(id) },
+    })
+    return NextResponse.json(post)
+}
